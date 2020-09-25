0cee0755a86d065878ca6b519da022070ccfe49d
visa4 <antonino.visalli@gmail.com>
2020-09-25 15:15:25 +0200

    Fix old dependences

--- localize/localize-mixin.d.ts
+++ localize/localize-mixin.d.ts
@@ -1,3 +1,4 @@
+import '@polymer/iron-ajax/iron-ajax.js';
 /**
  *
  * @type {Function}
@@ -13,6 +14,23 @@ export declare const LocalizeMixin: (superClass: any) => {
          * @param evt
          */
         changeLanguage(evt: any): void;
+        /**
+         *
+         * @param path
+         * @param language
+         * @param merge
+         */
+        loadResources(path: any, language: any, merge: any): void;
+        __onRequestResponse(event: any, language: any, merge: any): void;
+        /**
+         * @param proto
+         * @private
+         */
+        __checkLocalizationCache(proto: any): void;
+        /**
+         Returns a computed `localize` method, based on the current `language`.
+         */
+        __computeLocalize(language: any, resources: any, formats: any): any;
     };
     [x: string]: any;
     readonly properties: {
@@ -24,6 +42,55 @@ export declare const LocalizeMixin: (superClass: any) => {
             readOnly: boolean;
             notify: boolean;
             observer: string;
+        };
+        /**
+         * @string
+         */
+        language: {
+            type: StringConstructor;
+        };
+        /**
+         * @type object
+         * @description
+            The dictionary of localized messages, for each of the languages that
+            are going to be used. See http://formatjs.io/guides/message-syntax/ for
+            more information on the message syntax.
+
+            For example, a valid dictionary would be:
+            this.resources = {
+                'en': { 'greeting': 'Hello!' }, 'fr' : { 'greeting': 'Bonjour!' }
+            }
+         */
+        resources: {
+            type: ObjectConstructor;
+        };
+        formats: {
+            type: ObjectConstructor;
+            value: () => {};
+        };
+        /**
+         If true, will use the provided key when
+         the translation does not exist for that key.
+         */
+        useKeyIfMissing: {
+            type: BooleanConstructor;
+            value: boolean;
+        };
+        /**
+         Translates a string to the current `language`. Any parameters to the
+         string should be passed in order, as follows:
+         `localize(stringKey, param1Name, param1Value, param2Name, param2Value)`
+         */
+        localize: {
+            type: FunctionConstructor;
+            computed: string;
+        };
+        /**
+         If true, will bubble up the event to the parents
+         */
+        bubbleEvent: {
+            type: BooleanConstructor;
+            value: boolean;
         };
     };
 };
--- localize/localize-mixin.ts
+++ localize/localize-mixin.ts
@@ -1,7 +1,7 @@
-import {AppLocalizeBehavior} from '@polymer/app-localize-behavior/app-localize-behavior.js';
 import {Listener} from '@dsign/library/src/event/Listener';
 import {Localize} from '@dsign/library/src/localize/Localize';
-import {mixinBehaviors} from '@polymer/polymer/lib/legacy/class.js';
+import '@polymer/iron-ajax/iron-ajax.js';
+import IntlMessageFormat from 'intl-messageformat'
 
 /**
  *
@@ -9,7 +9,7 @@ import {mixinBehaviors} from '@polymer/polymer/lib/legacy/class.js';
  */
 export const LocalizeMixin = (superClass) => {
 
-    return class extends mixinBehaviors([AppLocalizeBehavior], superClass)  {
+    return class extends superClass {
 
         static get properties() {
             return {
@@ -21,7 +21,74 @@ export const LocalizeMixin = (superClass) => {
                     readOnly: true,
                     notify: true,
                     observer: 'changedLocalizeService'
-                }
+                },
+
+                /**
+                 * @string
+                 */
+                language: {
+                    type: String
+                },
+
+                /**
+                 * @type object
+                 * @description
+                    The dictionary of localized messages, for each of the languages that
+                    are going to be used. See http://formatjs.io/guides/message-syntax/ for
+                    more information on the message syntax.
+
+                    For example, a valid dictionary would be:
+                    this.resources = {
+                        'en': { 'greeting': 'Hello!' }, 'fr' : { 'greeting': 'Bonjour!' }
+                    }
+                 */
+                resources: {type: Object},
+
+                formats: {
+                    type: Object,
+                    value: function() {
+                        return {
+                        }
+                    }
+                },
+
+                /**
+                 If true, will use the provided key when
+                 the translation does not exist for that key.
+                 */
+                useKeyIfMissing: {
+                    type: Boolean,
+                    value: false
+                },
+
+                /**
+                 Translates a string to the current `language`. Any parameters to the
+                 string should be passed in order, as follows:
+                 `localize(stringKey, param1Name, param1Value, param2Name, param2Value)`
+                 */
+                localize: {
+                    type: Function,
+                    computed: '__computeLocalize(language, resources, formats)'
+                },
+
+                /**
+                 If true, will bubble up the event to the parents
+                 */
+                bubbleEvent: {type: Boolean, value: false}
+
+            };
+        }
+
+        /**
+         *
+         */
+        constructor() {
+            super();
+            this.__localizationCache = {
+                requests: {}, /* One iron-request per unique resources path. */
+                messages: {}, /* Unique localized strings. Invalidated when the language,
+                     formats or resources change. */
+                ajax: null    /* Global iron-ajax object used to request resource files. */
             };
         }
 
@@ -42,6 +109,138 @@ export const LocalizeMixin = (superClass) => {
          */
         changeLanguage(evt) {
             this.language = evt.data.language;
+        }
+
+        /**
+         *
+         * @param path
+         * @param language
+         * @param merge
+         */
+        loadResources(path, language, merge) {
+            var proto = this.constructor.prototype;
+
+            // Check if localCache exist just in case.
+            this.__checkLocalizationCache(proto);
+
+            // If the global ajax object has not been initialized, initialize and cache
+            // it.
+            var ajax = proto.__localizationCache.ajax;
+            if (!ajax) {
+                ajax = proto.__localizationCache.ajax =
+                    document.createElement('iron-ajax');
+            }
+
+            var request = proto.__localizationCache.requests[path];
+
+            function onRequestResponse(event) {
+                this.__onRequestResponse(event, language, merge);
+            }
+
+            if (!request) {
+                ajax.url = path;
+                var request = ajax.generateRequest();
+
+                request.completes.then(
+                    onRequestResponse.bind(this), this.__onRequestError.bind(this));
+
+                // Cache the instance so that it can be reused if the same path is loaded.
+                proto.__localizationCache.requests[path] = request;
+            } else {
+                request.completes.then(
+                    onRequestResponse.bind(this), this.__onRequestError.bind(this));
+            }
+        }
+
+        __onRequestResponse(event, language, merge) {
+            var propertyUpdates = {resources: {}};
+            var newResources = event.response;
+            if (merge) {
+                if (language) {
+                    propertyUpdates.resources = Object.assign({}, this.resources || {});
+                    propertyUpdates['resources.' + language] =
+                        Object.assign(propertyUpdates.resources[language] || {}, newResources);
+                } else {
+                    propertyUpdates.resources = Object.assign(this.resources, newResources);
+                }
+            } else {
+                if (language) {
+                    propertyUpdates.resources = {};
+                    propertyUpdates.resources[language] = newResources;
+                    propertyUpdates['resources.' + language] = newResources;
+                } else {
+                    propertyUpdates.resources = newResources;
+                }
+            }
+            if (this.setProperties) {
+                this.setProperties(propertyUpdates);
+            } else {
+                for (var key in propertyUpdates) {
+                    this.set(key, propertyUpdates[key]);
+                }
+            }
+            this.fire(
+                'app-localize-resources-loaded', event, {bubbles: this.bubbleEvent});
+        }
+
+        /**
+         * @param proto
+         * @private
+         */
+        __checkLocalizationCache(proto) {
+            // do nothing if proto is undefined.
+            if (proto === undefined)
+                return;
+
+            // In the event proto not have __localizationCache object, create it.
+            if (proto['__localizationCache'] === undefined) {
+                proto['__localizationCache'] = {requests: {}, messages: {}, ajax: null};
+            }
+        }
+
+        /**
+         Returns a computed `localize` method, based on the current `language`.
+         */
+        __computeLocalize(language, resources, formats) {
+            var proto = this.constructor.prototype;
+
+            // Check if localCache exist just in case.
+            this.__checkLocalizationCache(proto);
+
+            // Everytime any of the parameters change, invalidate the strings cache.
+            if (!proto.__localizationCache) {
+                proto['__localizationCache'] = {requests: {}, messages: {}, ajax: null};
+            }
+            proto.__localizationCache.messages = {};
+
+            return function() {
+                var key = arguments[0];
+                if (!key || !resources || !language || !resources[language])
+                    return;
+
+                // Cache the key/value pairs for the same language, so that we don't
+                // do extra work if we're just reusing strings across an application.
+                var translatedValue = resources[language][key];
+
+                if (!translatedValue) {
+                    return this.useKeyIfMissing ? key : '';
+                }
+
+                var messageKey = key + translatedValue;
+                var translatedMessage = proto.__localizationCache.messages[messageKey];
+
+                if (!translatedMessage) {
+                    translatedMessage = new IntlMessageFormat(translatedValue, language, formats);
+                    proto.__localizationCache.messages[messageKey] = translatedMessage;
+                }
+
+                var args = {};
+                for (var i = 1; i < arguments.length; i += 2) {
+                    args[arguments[i]] = arguments[i + 1];
+                }
+
+                return translatedMessage.format(args);
+            }.bind(this);
         }
     }
 };
